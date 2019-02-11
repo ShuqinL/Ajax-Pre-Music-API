261f2f419319905df20739412bf10bbd1bcaeaec
L <reneelsq@hotmail.com>
2019-02-11 15:40:43 +0100

    movie search finished

---
+++ index.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+         
+            
+
+            $("#searchButton").click(function(){
+                let searchField = $("#search").val();
+                let url="http://www.omdbapi.com/?s="+searchField+"&apikey=78c2fa05";
+
+                fetch(url)
+               .then((res)=>res.json())
+               .then((data)=>{
+                   let searchResults = data.Search;
+                   for(let i=0; i<searchResults.length;i++){
+                       let title = searchResults[i].Title;
+                       let year=searchResults[i].Year;
+                       let id=searchResults[i].imdbID;
+                       let type=searchResults[i].Type;
+                       let poster=searchResults[i].Poster;
+                       // make a dropdown list with the search results
+                       $("ul").append(`<li class="list-group-item"><a href="movieinfo.html?id=${id}">${title} ${year}</a></li>`);   
+
+                   }
+                   
+               })
+               .catch((err)=>console.log(err));
+            })
+        })
---
+++ movieinfo.js
@@ -0,0 +1,48 @@
+ $(document).ready(function(){
+     //make a back button
+       $("#backButton").click(function(){
+           window.history.back();
+       })
+     //get the movie id from url
+       const urlParams = new URLSearchParams(window.location.search);
+       const myId = urlParams.get("id");
+       console.log(myId);
+       
+        
+        fetch("http://www.omdbapi.com/?i="+myId+"&apikey=78c2fa05")
+        .then((res)=>res.json())
+        .then((data)=>{
+            let title = data.Title;
+            let year = data.Year;
+            let release = data.Released;
+            let duration = data.Runtime;
+            let genre = data.Genre;
+            let rate = data.Rated;
+            let imdbId = data.imdbID;
+            let director = data.Director;
+            let writer = data.Writer;
+            let actors = data.Actors;
+            let awards = data.Awards;
+            let poster = data.Poster;
+            let plot = data.Plot;
+            $("#title").text(title);
+            $("#year").text(year);
+            $("#release").text(release);
+            $("#duration").text(duration);
+            $("#genre").text(genre);
+            $("#rate").text(rate);
+            $("#imdbId").text(imdbId);
+            $("#director").text(director);
+            $("#writer").text(writer);
+            $("#actors").text(actors);
+            $("#awards").text(awards);
+            $("#plot").text(plot);
+            $("#poster").html("<img src=\""+poster+"\"/>");
+          
+         console.log(poster);
+         console.log(data);
+        }) 
+        .catch((err)=>console.log(err));
+        
+      
+    })
